87b726908ebf2707132c9b509a3533854461bc5b
moath <moaalnajjar@gmail.com>
2017-07-26 19:31:40 +0300

    i maked responsive design

---
+++ main.js
@@ -0,0 +1,16 @@
+// var clicking = document.getElementById('move');
+// var form = document.getElementById('form');
+
+// clicking.addEventListener('click',function(){
+
+// 			form.style.height = "45vh";
+// 			 form.style.transition = "all 1.5s ease";
+			
+// });
+
+// // clicking.addEventListener('click',function(){
+
+// // 			// form.style.height = "45vh";
+// // 			form.style.transition = "all 1.5s ease";
+			
+// // });
