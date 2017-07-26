52255052f9b62f2ba802d33fa2348a6809b5912d
samer elaila <samer-elaila@live.com>
2017-07-26 22:35:01 +0300

    upload files including bugs Relates #10

--- scripts/script.js
+++ scripts/script.js
@@ -1,108 +1,4 @@
-// Get Character short Bio From Game of Thrones API
-function getFromGOT(name) {
-  var xhr = new XMLHttpRequest();
-  var character = {};
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var data = {};
-      if (xhr.responseText !== "")
-        data = JSON.parse(xhr.responseText);
-      // console.log(data);
-       character = {
-        name: data.data.name,
-        gender: data.data.male,
-        culture: data.data.culture,
-        house: data.data.house,
-        books: data.data.books,
-        titles: data.data.titles
-      };
-      // console.log(character);
-      characterInfo = character;
-      console.log("This is From GOT API", character);
-    };
-  };
-  xhr.open("GET", 'https://api.got.show/api/characters/'+name, true);
-  xhr.send();
-  // return character;
-};
-var characterInfo = {};
-// getFromGOT();
-
-var ids = [
-   269623,
-   466865,
-   340200,
-   458492,
-   335382,
-   269625,
-   458480,
-   269626,
-   323452,
-   430070,
-   430068,
-   458499,
-   458500,
-   322484
-];
-
-var characters = {};
-var realName = "";
-
-//Get Actor name From TMDB API
-function getFromTMDB(characterName, callback){
-  ids.map(function(id){
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-      if (xhr.readyState == 4 && xhr.status == 200){
-        var data = JSON.parse(xhr.responseText);
-        data.cast.map(function(val){
-          characters[val.character.trim().replace(/(voice)/g, "").trim().replace(/ /g,"").replace(/[()\']/g, "")] = val.name;
-          readName = val.name;
-        });
-      };
-    };
-    var URL = "http://api.themoviedb.org/3/movie/"+ id +"/casts?api_key=bfd5274ec186e4bf6e99f1d3b76cdb1b";
-    xhr.open("GET" ,URL , true);
-    xhr.send();
-  });
-
-  characterName = characterName.replace(/[_ ]/g, "");
-  console.log(characterName);
-
-  if(callback)
-   return callback;
-}
-
-// getFromTMDB("Bran Stark");
-
-// Get Actor description From Wiki media api
-function getFromWiki(realName) {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var data = JSON.parse(xhr.responseText);
-      console.log(data);
-    }
-  }
-  xhr.open("GET", "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + realName + "&format=json", true);
-  xhr.setRequestHeader( 'Api-User-Agent', 'Example/1.0' );
-  xhr.send();
-}
-
-function process(fakeName){
-  getFromGOT(fakeName);
-  getFromTMDB(fakeName, getFromWiki("KitHarington"));
-  getImage("Jon Snow");
-}
-
-function getImage(name){
-  name = name.trim().replace(/[\s\'_]/g,"")
-};
-
-// getFromWiki("AidanGillen");
-
-process("Jon Snow");
-
+// Variables
 var images = {
   PetyrBaelishLittlefinger: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s7/peytr-baelish-littlefinger-512x512.jpg",
   BrienneofTarth: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s7/brienne-of-tarth-512x512.jpg",
@@ -202,3 +98,118 @@ var images = {
   MargaeryTyrell: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s5/margarey-tyrell-512x512.jpg",
   Ygritte: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s5/ygritte-512x512.jpg"
 }
+var ids = [269623,466865,340200,458492,335382,269625,458480,269626,323452,430070,430068,458499,458500,322484];
+var characterInfo = {};
+var characters = {};
+var realName = "";
+var str = document.querySelector("input[type='text']");
+
+// Get Character short Bio From Game of Thrones API
+function getFromGOT(name) {
+  var xhr = new XMLHttpRequest();
+  var character = {};
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var data = {};
+      if (xhr.responseText !== "")
+        data = JSON.parse(xhr.responseText);
+       character = {
+        name: data.data.name,
+        gender: data.data.male,
+        culture: data.data.culture,
+        house: data.data.house,
+        books: data.data.books,
+        titles: data.data.titles
+      };
+      characterInfo = character;
+      console.log("This is From GOT API", characterInfo);
+      document.querySelector(".character-name").textContent = "Name: " + characterInfo.name;
+      var gender = "female";
+      if(characterInfo.gender)  gender = "male";
+      document.querySelector(".character-gender").textContent = "Gender: " + gender;
+      var culture = "unknown";
+      if(characterInfo.culture) culture = characterInfo.culture;
+      document.querySelector(".character-culture").textContent = "Culture: " + culture;
+      var house = "unknown";
+      if(characterInfo.house) house = characterInfo.house;
+      document.querySelector(".character-house").textContent = "House: " + house;
+      // Name Gender Culture House
+    };
+  };
+  xhr.open("GET", 'https://api.got.show/api/characters/'+name, true);
+  xhr.send();
+};
+
+//Get Actor name From TMDB API
+function getFromTMDB(characterName, callback){
+  ids.map(function(id){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+      if (xhr.readyState == 4 && xhr.status == 200){
+        var data = JSON.parse(xhr.responseText);
+        data.cast.map(function(val){
+          characters[val.character.trim().replace(/(voice)/g, "").trim().replace(/ /g,"").replace(/[()\']/g, "")] = val.name;
+          // alert(realName);
+        });
+      };
+    };
+    var URL = "http://api.themoviedb.org/3/movie/"+ id +"/casts?api_key=bfd5274ec186e4bf6e99f1d3b76cdb1b";
+    xhr.open("GET" ,URL , true);
+    xhr.send();
+  });
+  console.log('This is Characters Console.log ', characters);
+  characterName = characterName.replace(/[_ ]/g, "");
+  realName = characters[characterName];
+  // alert(realName);
+  // alert(characterName);
+  console.log(characterName);
+  if(callback)
+   return setTimeout(callback, 1000);
+}
+
+
+// Get Actor description From Wiki media api
+function getFromWiki(realName) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var data = JSON.parse(xhr.responseText);
+      // console.log(data);
+      document.querySelector('.info2').innerHTML = data.query.search[0].snippet;
+      // console.log(data.query.search);
+    }
+  }
+  // if(!realName){
+  //   document.querySelector(".info2").textContent = "Oops!! \nActor Not Found !!!!";
+  // }
+  xhr.open("GET", "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + realName + "&format=json", true);
+  xhr.setRequestHeader( 'Api-User-Agent', 'Example/1.0' );
+  xhr.send();
+}
+
+// getFromWiki('Kit Harington');
+
+// Get Image URL From Image Object
+function getImage(name){
+  name = name.replace(/[\s\'_]/g,"")
+  console.log("getFromImage Function ==> " + images[name]);
+  document.querySelector("img").src = images[name];
+};
+
+// Assemble All Functions Together
+function process(fakeName){
+  getFromGOT(fakeName);
+  getFromTMDB(fakeName, function(){
+    if(realName)
+      // alert('person identified');
+      getFromWiki(realName);
+  });
+  getImage(fakeName);
+}
+
+document.querySelector("#form").addEventListener('submit', function(event){
+  event.preventDefault();
+  process(str.value);
+});
+
+// document.querySelector("img").src = images["JonSnow"];
